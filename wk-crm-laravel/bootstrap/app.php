0f802437fba93a24fe87bf91692fe079b17cd5b7
Cristian Marques <cristianms.awk@gmail.com>
2025-11-17 13:30:58 -0300

    fix: Add CORS configuration for Angular frontend

--- wk-crm-laravel/bootstrap/app.php
+++ wk-crm-laravel/bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(remove: [
             \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
         ]);
+        
+        // Enable CORS for API routes
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
